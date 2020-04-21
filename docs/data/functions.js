92f7de5fc4a04b81f46808d231c5be7c8a53e319
Mai Ho <maddieho1292@gmail.com>
2020-04-22 01:33:16 +0700

    index.html

--- docs/data/functions.js
+++ docs/data/functions.js
@@ -7,7 +7,7 @@ var styles = {'solid':'fas','regular':'far','brands':'fab'};
 var values = Object.entries(icons).reduce((acc, [fk, fv], i) => (acc[i] = {name: fk, label: fv.label, styles: fv.styles, unicode: fv.unicode, prefix: styles[fv.styles[0]], terms: fv.search.terms}, acc), []); return values;
 }
 
-function GetSelect() {
+function getSelect() {
 var temp = {
   "names": $table.bootstrapTable('getAllSelections').map(row => row.name),
   "text": function () {
@@ -15,5 +15,5 @@ var temp = {
   }
 }
 var text = temp.text() + temp.names.join(', ');
-$selects.html(text);
+return text;
 }
