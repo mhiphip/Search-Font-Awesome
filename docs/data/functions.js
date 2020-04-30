f3e1bef0ce193673ee82fd7de50af0bfe98eb910
Mai Ho <maddieho1292@gmail.com>
2020-04-30 07:32:15 +0700

    colorpicker.html

--- docs/data/functions.js
+++ docs/data/functions.js
@@ -2,10 +2,22 @@ function onlyUnique(value, index, self) {
 return self.indexOf(value) === index;
 }
 
+// icons
 function getIcons(data) {
+var exts = {prefix: function() {return this.prefixes[0]},
+class: function() {return Mustache.render("<i class=\'{{&prefix}} fa-{{&name}}\'></i>", this)}
+};
+
 var styles = {'solid':'fas','regular':'far','brands':'fab'};
-var values = Object.entries(data).reduce((acc, [fk, fv], i) => (acc[i] = {name: fk, label: fv.label, styles: fv.styles, unicode: fv.unicode, prefix: fv.styles.map(st => styles[st]), terms: fv.search.terms}, acc), []);
+var values = Object.entries(data).reduce((acc, [fk, fv], i) => (acc[i] = {name: fk, label: fv.label, styles: fv.styles, unicode: fv.unicode, prefixes: fv.styles.map(st => styles[st]), terms: fv.search.terms}, acc[i] = Object.assign(acc[i], exts), acc), []);
 return values;
+}
+
+// submit icons
+function SubmitIcons() {
+var $review = $("#review");
+var icons = $table.bootstrapTable('getAllSelections');
+RenderTemp("icons", {icons: icons}, function(render){ $review.find("> div").append(render);});
 }
 
 function getSelect() {
@@ -19,21 +31,12 @@ var text = temp.text() + temp.names.join(', ');
 return text;
 }
 
-function CreateDivs(data) {
-var temp = {
-  "list": "{{#pages}}\n<li class=\"nav-item\"><a class=\"nav-link\" data-toggle=\"tab\" href=\"#{{&id}}\">{{&name}}</a></li>\n{{/pages}}",
-  "tab": "{{#pages}}<div class=\"tab-pane fade\" id=\"{{&id}}\">{{&html}}</div>{{/pages}}"
-}
-temp.list = Mustache.render(temp.list, data);
-temp.tab = Mustache.render(temp.tab, data);
-return temp;
-}
 
 function httpRq(data) {
 var http = new XMLHttpRequest();
 http.onreadystatechange = function() {
    if(http.readyState == 4 && http.status == 200) {
-   data.callback(JSON.parse(http.responseText));
+    data.callback(JSON.parse(http.responseText));
    }
 }
 
@@ -52,4 +55,12 @@ swatch.attr('data-value', colorStr)
 .attr('title', colorStr).find('> i')
 .css('background-color', colorStr);
  });});
+}
+
+function RenderTemp(tk, data, callback) {
+ $.get("data/template.json", function(temp) {
+ var render = Mustache.render(temp[tk], data);
+ console.log(temp[tk] + render);
+  callback(render);
+ });
 }
