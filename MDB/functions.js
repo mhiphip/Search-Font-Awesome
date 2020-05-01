0db57b9a3cf3ec5f84686fba6ce51732319e2495
Mai Ho <maddieho1292@gmail.com>
2020-05-01 14:23:03 +0700

    bootstrap.min.css

---
+++ MDB/functions.js
@@ -0,0 +1,19 @@
+function onlyUnique(value, index, self) {
+return self.indexOf(value) === index;
+}
+
+function getIcons(icons) {
+var styles = {'solid':'fas','regular':'far','brands':'fab'};
+var values = Object.entries(icons).reduce((acc, [fk, fv], i) => (acc[i] = {name: fk, label: fv.label, styles: fv.styles, unicode: fv.unicode, prefix: styles[fv.styles[0]], terms: fv.search.terms}, acc), []); return values;
+}
+
+function GetSelect() {
+var temp = {
+  "names": $table.bootstrapTable('getAllSelections').map(row => row.name),
+  "text": function () {
+    return (!this.names.length) ? '' : `You selected <strong>${this.names.length}</strong> icons:\n`;
+  }
+}
+var text = temp.text() + temp.names.join(', ');
+$selects.html(text);
+}
