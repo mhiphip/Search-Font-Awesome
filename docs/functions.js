77bbe355b63de0f3d5b9861921b8d4c604c3d902
Mai Ho <maddieho1292@gmail.com>
2023-12-30 14:59:06 -0800

    s

--- docs/functions.js
+++ docs/functions.js
@@ -105,7 +105,7 @@ function ColorInput(format, callback) {
   if (format.input == "random") {
   var data = {'url':'http://colormind.io/api/','method':'PUT','data':{'model':'ui'}};
 
-$.put(data.url,JSON.stringify(data.data), function(data,status,xhr){}, "json")
+$.post(data.url,JSON.stringify(data.data), function(data,status,xhr){}, "json")
 .then(data => {
   format.values = data.result.map(value => RGBToHex(`rgb(${value})`));
   callback(format);
